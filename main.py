c07009e35d8504c77c57209258a375d58123aba6
darkcru <anna.sidorenko.biz@gmail.com>
2024-01-09 01:13:10 +0200

    Tasks1-2_Max_Min_Ave_Meters_Miles_Inches_Yards

--- main.py
+++ main.py
@@ -38,52 +38,52 @@
 # print(f"Result: {result}")
 
 #Task1
-# a = int(input("Enter first number: "))
-# b = int(input("Enter second number: "))
-# c = int(input("Enter third number: "))
-#
-#
-# if a > b and a > c:
-#    print(a)
-# elif b > a and b > c:
-#    print(b)
-# elif c > a and c > b:
-#    print(c)
-# elif a == b and b > c:
-#    print(b)
-# elif a == b and b < c:
-#    print(c)
-# elif c == b and c > a:
-#    print(c)
-# elif c == b and c < a:
-#    print(a)
-# elif a == c and c > b:
-#    print(a)
-# elif a == c and c < b:
-#    print(b)
-# else:
-#    print(a)
-#
-# if a < b and a < c:
-#    print(a)
-# elif b < a and b < c:
-#    print(b)
-# elif c < a and c < b:
-#    print(c)
-# elif a == b and b < c:
-#    print(b)
-#
-# elif c == b and c < a:
-#    print(c)
-#
-# elif a == c and c < b:
-#    print(c)
-#
-# else:
-#    print(b)
-#
-# result = (a + b + c)/3
-# print(f"Result: {result}")
+a = int(input("Enter first number: "))
+b = int(input("Enter second number: "))
+c = int(input("Enter third number: "))
+
+
+if a > b and a > c:
+   print(a)
+elif b > a and b > c:
+   print(b)
+elif c > a and c > b:
+   print(c)
+elif a == b and b > c:
+   print(b)
+elif a == b and b < c:
+   print(c)
+elif c == b and c > a:
+   print(c)
+elif c == b and c < a:
+   print(a)
+elif a == c and c > b:
+   print(a)
+elif a == c and c < b:
+   print(b)
+else:
+   print(a)
+
+if a < b and a < c:
+   print(a)
+elif b < a and b < c:
+   print(b)
+elif c < a and c < b:
+   print(c)
+elif a == b and b < c:
+   print(b)
+
+elif c == b and c < a:
+   print(c)
+
+elif a == c and c < b:
+   print(c)
+
+else:
+   print(b)
+
+result = (a + b + c)/3
+print(f"Result: {result}")
 
 #Task2
 number = int(input("Enter distance in meters: "))
